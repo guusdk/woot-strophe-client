bbef32f1f16b6dd51f72984c0b172bf76eae2157
Guus der Kinderen <guus.der.kinderen@gmail.com>
2017-05-04 20:42:08 +0200

    Removing unused loaders.

--- webpack.config.js
+++ webpack.config.js
@@ -10,30 +10,6 @@ module.exports = {
     module: {
         loaders: [
             {test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/},
-            {test: /\.css$/, loader: "style-loader!css-loader"},
-            // inline base64 URLs for <=8k images, direct URLs for the rest
-            //{ test: /\.(png|jpg)$/, loader: 'url-loader?limit=8192'},
-            // helps to load bootstrap's css.
-            {
-                test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
-                loader: 'url-loader?limit=10000&minetype=application/font-woff'
-            },
-            {
-                test: /\.woff2$/,
-                loader: 'url-loader?limit=10000&minetype=application/font-woff'
-            },
-            {
-                test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
-                loader: 'url-loader?limit=10000&minetype=application/octet-stream'
-            },
-            {
-                test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
-                loader: 'file-loader'
-            },
-            {
-                test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-                loader: 'url-loader?limit=10000&minetype=image/svg+xml'
-            }
         ]
     },
     devtool: 'source-map'
